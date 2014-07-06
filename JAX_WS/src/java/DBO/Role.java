daaec56dc906de9470a69f19f1b4ba489eca2c16
George Kfoury <joujproduction@gmail.com>
2014-07-06 16:24:20 +0300

    Commit Initial du CRUD avec la connection a MySQL et le skeleton de la class qui sera un web-service du CRUD

---
+++ JAX_WS/src/java/DBO/Role.java
@@ -0,0 +1,12 @@
+package DBO;
+
+/**
+ *
+ * @author jouj
+ */
+public class Role extends Tools.CRUD {
+    public Role() {
+        super("role");
+    }
+    
+}
---
+++ JAX_WS/src/java/gss/test.java
@@ -0,0 +1,154 @@
+package gss;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import DBO.*;
+import Tools.Func;
+import Tools.QueryCriteria;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+
+/**
+ *
+ * @author jouj
+ */
+@WebServlet(name = "test", urlPatterns = {"/test"})
+public class test extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, NamingException, SQLException {
+                response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+        Role r = new Role();
+        HashMap<String,String> values = new HashMap<String, String>();
+//        values.put("role_id", "1");
+        values.put("role_name", "test2");
+        values.put("role_desc", "description2");
+        values.put("role_time_stamp", Func.NOW());
+        out.println(values);
+       // r.Create(values);
+        
+            QueryCriteria Q = new QueryCriteria("role_name", "test2", QueryCriteria.Operand.EQUALS);
+            ArrayList<QueryCriteria> Q2 = new ArrayList<QueryCriteria>();
+            Q2.add(Q);
+            ResultSet rs = r.Read(Q2, new ArrayList<String>());
+            System.out.println(rs.getFetchSize());
+            while (rs.next()) {
+                out.println("<h1>First Name is " + rs.getString("role_name") + "</h1>");
+            }
+            
+//        InitialContext ctx = new InitialContext();
+//        DataSource ds = (DataSource) ctx.lookup("jdbc/mysql");
+//
+//        Connection conn = ds.getConnection();
+//        Statement stmt = conn.createStatement();
+//        //select * from utilisateur
+//        
+//        ResultSet rs = stmt.executeQuery("select * from utilisateur");
+//
+//            out.println("<html>");
+//            out.println("<head>");
+//            out.println("<title>Servlet POC on Glassfish</title>");
+//
+//            while (rs.next()) {
+//                out.println("<h1>First Name is " + rs.getString("idUtilisateur") + "</h1>");
+//            }
+//            out.println("</body>");
+//            out.println("</html>");
+//
+//            stmt.close();
+//            stmt = null;
+//            conn.close();
+//            conn = null;
+        
+
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet test</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet test at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (NamingException ex) {
+            Logger.getLogger(test.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(test.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (NamingException ex) {
+            Logger.getLogger(test.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(test.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
